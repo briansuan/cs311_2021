851cc3ada245f5eeefb3d07f775457b4474f5545
Ubuntu <ubuntu@ip-172-31-22-97.us-west-1.compute.internal>
2021-11-12 19:52:01 +0000

    Brian Suan assignment 4

--- assignment4/briansuan_ann.py
+++ assignment4/briansuan_ann.py
@@ -1,16 +1,20 @@
 import random
 import string
 
-class Node:
 
+class Node:
+    
+    # Constructor: creates an array for children and weights of the connection for each children
+    # each node is represented as a sequence of  3 random ascii characters 
     def __init__(self):
         self.children = []
         
-        for i in range(3):
-            self.my_node = ''.join([random.choice(string.ascii_letters)])
+        self.my_node = ''.join([random.choice(string.ascii_letters) for i in range (3)])
 
         self.children_connection_weights = []
-
+    
+    # A recursive function to make children, break out of the recursion if the current layer is at the last layer, 
+    # the last layer will be when the current_layer is the same as the length of the nodes_per_layer_map array 
     def make_children(self, current_layer, nodes_per_layer_map):
         if current_layer == len(nodes_per_layer_map):
             return
@@ -25,10 +29,12 @@ class Node:
         for i in range(1, len(self.children)):
             self.children[i].children = first_born.children[:]
 
-
+    # A function to adjust the weights of each child
     def adjust_child_weights(self):
+
         if len(self.children) == 0:
             return
+
         self.children_connection_weights = []
 
         for i in range(len(self.children)):
@@ -36,6 +42,7 @@ class Node:
 
             self.children[i].adjust_child_weights()
     
+    # Print out the nodes 
     def OUTPUT_children(self, layer):
         indent = '    ' * layer
 
@@ -43,6 +50,7 @@ class Node:
             print(f"{indent}{self.my_node}")
 
         print(f"{indent}{self.my_node} is connected to ")
+
         for i in range(len(self.children)):
             self.children[i].OUTPUT_children(layer + 1)
 
@@ -50,19 +58,25 @@ class Node:
                 print(f"{indent}with weight {self.children_connection_weights[i]} ")
 
 
-
+# create an array of nodes
 nodes = []
 
+# create an instance of the Node() class called master_node()
 master_node = Node()
 
+# Create another instance of the node class for my_first_node
 my_first_node = Node()
 
+# The first layer will contain 4 nodes, second will contain 3 nodes, and last layer will contain 2 nodes
 NODE_COUNT_PER_LAYER = [4,3,2]
 
+# Make the children for the first node
 my_first_node.make_children(1, NODE_COUNT_PER_LAYER)
 
+# The master node is the first node
 master_node.children.append(my_first_node)
 
+# Create the rest of the nodes
 for i in range(1, len(NODE_COUNT_PER_LAYER)):
     new_node = Node()
 
@@ -70,8 +84,12 @@ for i in range(1, len(NODE_COUNT_PER_LAYER)):
 
     master_node.children.append(new_node)
 
+
 master_node.OUTPUT_children(0)
+
+
 print("!! Set Weights !!")
+
 master_node.adjust_child_weights()
 
 master_node.OUTPUT_children(0)
